290175a58abf2ace8efa9c7b217033b831ef9bc3
Ciprian Ticu <ciprianticu@gmail.com>
2021-05-13 21:09:26 +0300

    Update OtherFile.java

--- OtherFile.java
+++ OtherFile.java
@@ -1,2 +1,7 @@
 public class OtherFile {
+  public static void alterPointer2(Rectangle r)
+	{	System.out.println("In method alterPointer. r " + r + "\n");
+		r = new Rectangle(5, 10, 30, 35);
+		System.out.println("In method alterPointer. r " + r + "\n");
+	}
 }
