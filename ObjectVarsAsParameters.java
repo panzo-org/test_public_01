c1d84d309c5ad0c4eb2e6c3a620f26fd7a177ed3
Ciprian Ticu <ciprianticu@gmail.com>
2023-04-10 12:05:20 +0300

    Update ObjectVarsAsParameters.java

--- ObjectVarsAsParameters.java
+++ ObjectVarsAsParameters.java
@@ -42,5 +42,11 @@ public class ObjectVarsAsParameters
                 r = new Rectangle(5, 10, 30, 35);
                 System.out.println("In method alterPointer. r " + r + "\n");
         }
+ 
+        public static void alterPointer3(Rectangle r)
+        {       System.out.println("In method alterPointer. r " + r + "\n");
+                r = new Rectangle(5, 10, 30, 35);
+                System.out.println("In method alterPointer. r " + r + "\n");
+        }
 	
 }
